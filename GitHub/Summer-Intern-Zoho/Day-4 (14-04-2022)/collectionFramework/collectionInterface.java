c73ae5feb7dedfa742c5496d72159bf5442a4a99
shyamsundar233 <81059580+shyamsundar233@users.noreply.github.com>
2022-04-14 12:27:56 +0530

    Tasks

---
+++ GitHub/Summer-Intern-Zoho/Day-4 (14-04-2022)/collectionFramework/collectionInterface.java
@@ -0,0 +1,17 @@
+package collectionFramework;
+
+import java.util.*;
+
+public class collectionInterface {
+
+    public static void main(String[] args){
+
+        Collection<Integer> collection = new ArrayList<Integer>();
+
+        collection.add(10);
+        collection.add(10);
+        System.out.println(collection);
+
+    }
+
+}
